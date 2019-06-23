e021dc1b3d4968457c6fe8c641fd3dd846a89110
nigamkartik962 <kartik.nigam@paytm.com>
2019-06-23 19:58:04 +0530

    insta-application: Adding login and register controller

--- insta-register/src/main/java/com/insta/register/model/UserData.java
+++
@@ -1,7 +0,0 @@
-package com.insta.register.model;
-
-import lombok.Data;
-
-@Data
-public class UserData {
-}
--- register/src/main/java/com/insta/register/controller/LoginRegsiterController.java
+++ register/src/main/java/com/insta/register/controller/LoginRegsiterController.java
@@ -1,23 +1,21 @@
 package com.insta.register.controller;
 
-import com.insta.register.model.UserData;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
 @Slf4j
+@AllArgsConstructor
 @RequestMapping("/intro")
-public class LoginAndRegisterController {
+public class LoginRegsiterController {
 
     @GetMapping("/register")
-    public String saveUser(
-           // @RequestBody UserData userData
+    public String registerUser(
+            // @RequestBody User userData
     ) {
-        return "Hello World!";
+        return "Hello, World!";
     }
 }
