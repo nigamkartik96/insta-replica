5c40cd3df1abacad24f3c6b0b6c8d4ab664e7383
Kartik <nigamkartik962@gmail.com>
2019-08-04 12:43:27 +0530

    Update LoginRegisterController.java

--- register/src/main/java/com/insta/register/controller/LoginRegisterController.java
+++ register/src/main/java/com/insta/register/controller/LoginRegisterController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//mongo "mongodb+srv://instaproject-s3q76.mongodb.net/test" --username kartik
-
 @RestController
 @Slf4j
 @AllArgsConstructor
